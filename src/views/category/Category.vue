364cc752c2d7b7f0667dae8068ed3823c2b13f4a
TMKZS <2739604881@qq.com>
2021-03-02 21:45:29 +0800

    更改

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,141 +1,93 @@
 <!--  -->
 <template>
-  <div class="wrapper">
-    <ul class="content">
-      <li>分类商品1</li>
-      <li>分类商品2</li>
-      <li>分类商品3</li>
-      <li>分类商品4</li>
-      <li>分类商品5</li>
-      <li>分类商品6</li>
-      <li>分类商品7</li>
-      <li>分类商品8</li>
-      <li>分类商品9</li>
-      <li>分类商品10</li>
-      <li>分类商品11</li>
-      <li>分类商品12</li>
-      <li>分类商品13</li>
-      <li>分类商品14</li>
-      <li>分类商品15</li>
-      <li>分类商品16</li>
-      <li>分类商品17</li>
-      <li>分类商品18</li>
-      <li>分类商品19</li>
-      <li>分类商品20</li>
-      <li>分类商品21</li>
-      <li>分类商品22</li>
-      <li>分类商品23</li>
-      <li>分类商品24</li>
-      <li>分类商品25</li>
-      <li>分类商品26</li>
-      <li>分类商品27</li>
-      <li>分类商品28</li>
-      <li>分类商品29</li>
-      <li>分类商品30</li>
-      <li>分类商品31</li>
-      <li>分类商品32</li>
-      <li>分类商品33</li>
-      <li>分类商品34</li>
-      <li>分类商品35</li>
-      <li>分类商品36</li>
-      <li>分类商品37</li>
-      <li>分类商品38</li>
-      <li>分类商品39</li>
-      <li>分类商品40</li>
-      <li>分类商品41</li>
-      <li>分类商品42</li>
-      <li>分类商品43</li>
-      <li>分类商品44</li>
-      <li>分类商品45</li>
-      <li>分类商品46</li>
-      <li>分类商品47</li>
-      <li>分类商品48</li>
-      <li>分类商品49</li>
-      <li>分类商品50</li>
-      <li>分类商品51</li>
-      <li>分类商品52</li>
-      <li>分类商品53</li>
-      <li>分类商品54</li>
-      <li>分类商品55</li>
-      <li>分类商品56</li>
-      <li>分类商品57</li>
-      <li>分类商品58</li>
-      <li>分类商品59</li>
-      <li>分类商品60</li>
-      <li>分类商品61</li>
-      <li>分类商品62</li>
-      <li>分类商品63</li>
-      <li>分类商品64</li>
-      <li>分类商品65</li>
-      <li>分类商品66</li>
-      <li>分类商品67</li>
-      <li>分类商品68</li>
-      <li>分类商品69</li>
-      <li>分类商品70</li>
-      <li>分类商品71</li>
-      <li>分类商品72</li>
-      <li>分类商品73</li>
-      <li>分类商品74</li>
-      <li>分类商品75</li>
-      <li>分类商品76</li>
-      <li>分类商品77</li>
-      <li>分类商品78</li>
-      <li>分类商品79</li>
-      <li>分类商品80</li>
-      <li>分类商品81</li>
-      <li>分类商品82</li>
-      <li>分类商品83</li>
-      <li>分类商品84</li>
-      <li>分类商品85</li>
-      <li>分类商品86</li>
-      <li>分类商品87</li>
-      <li>分类商品88</li>
-      <li>分类商品89</li>
-      <li>分类商品90</li>
-      <li>分类商品91</li>
-      <li>分类商品92</li>
-      <li>分类商品93</li>
-      <li>分类商品94</li>
-      <li>分类商品95</li>
-      <li>分类商品96</li>
-      <li>分类商品97</li>
-      <li>分类商品98</li>
-      <li>分类商品99</li>
-      <li>分类商品100</li>
-    </ul>
+  <div>
+
+      <nav-bar class="nav-bar"><div slot="center">商品分类</div>  </nav-bar>
+      <div class="flex-f">
+
+          <ul class="ul-f flex-s">
+            <li class="li-s"
+            :class="{active:currentIndex === index}"
+            v-for= '(item, index) in titles'
+            @click="itemClick(index)">{{item}}</li>
+          </ul>
+
+        <div  class="flex-s">
+          <h2>标题标题</h2>
+          <h2>标题标题</h2>
+          <h2>标题标题</h2>
+          <h2>标题标题</h2>
+          <h2>标题标题</h2>
+          <h2>标题标题</h2>
+        </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+
 import BScroll from 'better-scroll'
 
 export default {
   name:'Category',
   data () {
     return {
-      scroll : null,
+      titles: ['上衣','裤子','裙子','内衣','女鞋','男友','包包','运动','配饰','美妆','个护','家居','百货','母婴','食品'],
+      currentIndex: 0,
     };
   },
 
   mounted(){
-    this.scroll = new BScroll(document.querySelector('.wrapper'),{
 
-    })
   },
 
 
-  components: {},
+  components: {
+    NavBar
+  },
 
-  computed: {},
+  computed: {
+
+  },
 
 
-  methods: {}
+  methods: {
+    itemClick(index){
+      this.currentIndex = index;
+
+    }
+  }
 }
 
 </script>
 <style scoped>
-  .wrapper{
-    height: 150px;
-    background-color: red;
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
   }
+
+  .li-s {
+    background-color: #f6f6f6;
+    /* border: 1px solid red; */
+    width: 80px;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    font-size: 14px;
+  }
+  .active{
+    background-color: #fff;
+    border-left: 4px pink solid ;
+  }
+
+  .flex-f{
+    display: flex;
+  }
+
+  .flex-s{
+    flex: 1;
+  }
+
 </style>
